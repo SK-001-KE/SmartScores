42e4db2454867259724e3ba61fb86c1ee14de2b4
KARIUKI <SAMKARIUKY001@GMAIL.COM>
2025-10-28 01:53:10 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,53 +1,30 @@
-// Teacher Mean Score Tracker - Service Worker
 const CACHE_NAME = "teacher-mean-score-tracker-v1.0";
-const FILES_TO_CACHE = [
+const ASSETS = [
   "./",
   "./index.html",
-  "./style.css",
   "./app.js",
-  "./about.html",
   "./manifest.json",
   "./favicon.ico",
   "./icons/icon-192.png",
   "./icons/icon-512.png"
 ];
 
-// Install event: cache essential files
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
-  self.skipWaiting();
 });
 
-// Activate event: clear old caches
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keyList) => {
-      return Promise.all(
-        keyList.map((key) => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      );
-    })
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
   );
-  self.clients.claim();
 });
 
-// Fetch event: serve from cache, then update
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return (
-        response ||
-        fetch(event.request).catch(() =>
-          caches.match("./index.html")
-        )
-      );
-    })
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
   );
 });
