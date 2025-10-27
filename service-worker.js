5dd6529c2283a4b1ed74699d9dd2aa14c4e04312
KARIUKI <SAMKARIUKY001@GMAIL.COM>
2025-10-28 02:22:08 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,30 +1,50 @@
-const CACHE_NAME = "teacher-mean-score-tracker-v1.0";
-const ASSETS = [
+// SmartScores PWA Service Worker
+const CACHE_NAME = "smartscores-v1.0";
+const FILES_TO_CACHE = [
   "./",
   "./index.html",
   "./app.js",
   "./manifest.json",
-  "./favicon.ico",
   "./icons/icon-192.png",
   "./icons/icon-512.png"
 ];
 
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
   );
+  self.skipWaiting();
 });
 
-self.addEventListener("activate", e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
   );
+  self.clients.claim();
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return (
+        response ||
+        fetch(event.request).then((res) => {
+          return caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request.url, res.clone());
+            return res;
+          });
+        })
+      );
+    }).catch(() => caches.match("./index.html"))
   );
 });
