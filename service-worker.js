968f1f3ee08d3e4033a40abc854f50cba63ec6c0
KARIUKI <SAMKARIUKY001@GMAIL.COM>
2025-10-28 03:13:00 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,51 +1,47 @@
-// SmartScores PWA Service Worker
-const CACHE_NAME = "smartscores-v1.0";
+// SmartScores v2.0 Service Worker
+const CACHE_NAME = "smartscores-v2";
 const FILES_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./about.html",
-  "./app.js",
-  "./manifest.json",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png"
+  "/",
+  "/index.html",
+  "/style.css",
+  "/app.js",
+  "/favicon.png",
+  "/manifest.json",
+  "https://cdn.jsdelivr.net/npm/xlsx/dist/xlsx.full.min.js",
+  "https://cdn.jsdelivr.net/npm/jspdf@2.5.1/dist/jspdf.umd.min.js",
+  "https://cdn.jsdelivr.net/npm/jspdf-autotable@3.8.1/dist/jspdf.plugin.autotable.min.js",
 ];
 
-self.addEventListener("install", (event) => {
-  event.waitUntil(
+self.addEventListener("install", (evt) => {
+  evt.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
+      console.log("Pre-caching resources");
       return cache.addAll(FILES_TO_CACHE);
     })
   );
   self.skipWaiting();
 });
 
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keyList) => {
-      return Promise.all(
+self.addEventListener("activate", (evt) => {
+  evt.waitUntil(
+    caches.keys().then((keyList) =>
+      Promise.all(
         keyList.map((key) => {
           if (key !== CACHE_NAME) {
+            console.log("Removing old cache", key);
             return caches.delete(key);
           }
         })
-      );
-    })
+      )
+    )
   );
   self.clients.claim();
 });
 
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return (
-        response ||
-        fetch(event.request).then((res) => {
-          return caches.open(CACHE_NAME).then((cache) => {
-            cache.put(event.request.url, res.clone());
-            return res;
-          });
-        })
-      );
-    }).catch(() => caches.match("./index.html"))
+self.addEventListener("fetch", (evt) => {
+  evt.respondWith(
+    caches.match(evt.request).then((response) => {
+      return response || fetch(evt.request);
+    })
   );
 });
