486d60c4cf7b044591dd1297e58f649b8c7047e5
KARIUKI <SAMKARIUKY001@GMAIL.COM>
2025-10-28 13:56:37 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,29 +1,39 @@
-// SmartScores PWA Service Worker
-const CACHE_NAME = "smartscores-v1.0";
+// ===== SmartScores v2.0 PWA Service Worker =====
+const CACHE_NAME = "smartscores-v2.0";
 const FILES_TO_CACHE = [
   "./",
   "./index.html",
+  "./about.html",
   "./app.js",
   "./manifest.json",
+  "./favicon.png",
+  "./logo.png",
   "./icons/icon-192.png",
-  "./icons/icon-512.png"
+  "./icons/icon-512.png",
+  "https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js",
+  "https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js",
+  "https://cdnjs.cloudflare.com/ajax/libs/jspdf-autotable/3.5.28/jspdf.plugin.autotable.min.js"
 ];
 
+// Install Service Worker and cache files
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
+      console.log("[ServiceWorker] Caching app shell...");
       return cache.addAll(FILES_TO_CACHE);
     })
   );
   self.skipWaiting();
 });
 
+// Activate Service Worker and remove old caches
 self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches.keys().then((keyList) => {
       return Promise.all(
         keyList.map((key) => {
           if (key !== CACHE_NAME) {
+            console.log("[ServiceWorker] Removing old cache:", key);
             return caches.delete(key);
           }
         })
@@ -33,18 +43,14 @@ self.addEventListener("activate", (event) => {
   self.clients.claim();
 });
 
+// Fetch handler: serve cached content when offline
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
-      return (
-        response ||
-        fetch(event.request).then((res) => {
+      if (response) return response;
+
+      return fetch(event.request)
+        .then((res) => {
           return caches.open(CACHE_NAME).then((cache) => {
             cache.put(event.request.url, res.clone());
             return res;
-          });
-        })
-      );
-    }).catch(() => caches.match("./index.html"))
-  );
-});
