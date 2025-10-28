09b032a3aeb943d4a93205dfad21d69d1c97354c
KARIUKI <SAMKARIUKY001@GMAIL.COM>
2025-10-28 14:36:20 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,68 +1,22 @@
-// ===== SmartScores v2.0 PWA Service Worker =====
-const CACHE_NAME = "smartscores-v2.0";
-const FILES_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./about.html",
-  "./app.js",
-  "./manifest.json",
-  "./favicon.png",
-  "./logo.png",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png",
+const CACHE_NAME="smartscores-v2.0";
+const FILES_TO_CACHE=[
+  "./","./index.html","./about.html","./app.js","./manifest.json",
+  "./favicon.png","./icons/icon-192.png","./icons/icon-512.png",
+  "./js/jspdf.plugin.autotable.min.js",
   "https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js",
-  "https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js",
-  "https://cdnjs.cloudflare.com/ajax/libs/jspdf-autotable/3.5.28/jspdf.plugin.autotable.min.js"
+  "https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js"
 ];
 
-// Install Service Worker and cache files
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      console.log("[ServiceWorker] Caching app shell...");
-      return cache.addAll(FILES_TO_CACHE);
-    })
-  );
+self.addEventListener("install",e=>{
+  e.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(FILES_TO_CACHE)));
   self.skipWaiting();
 });
 
-// Activate Service Worker and remove old caches
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keyList => {
-      return Promise.all(
-        keyList.map(key => {
-          if (key !== CACHE_NAME) {
-            console.log("[ServiceWorker] Removing old cache:", key);
-            return caches.delete(key);
-          }
-        })
-      );
-    })
-  );
+self.addEventListener("activate",e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(key=>key!==CACHE_NAME?caches.delete(key):null))));
   self.clients.claim();
 });
 
-// Fetch handler: serve cached content when offline
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      if (response) return response;
-
-      return fetch(event.request)
-        .then(res => {
-          // Cache the new request
-          return caches.open(CACHE_NAME).then(cache => {
-            cache.put(event.request.url, res.clone());
-            return res;
-          });
-        })
-        .catch(() => {
-          // Fallback for navigation requests
-          if (event.request.mode === "navigate") {
-            return caches.match("./index.html");
-          }
-        });
-    })
-  );
+self.addEventListener("fetch",e=>{
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>caches.open(CACHE_NAME).then(c=>{c.put(e.request.url,res.clone()); return res;}))).catch(()=>caches.match("./index.html")));
 });
